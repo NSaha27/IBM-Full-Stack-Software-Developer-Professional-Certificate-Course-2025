4b4bbd8f776056679fdcec636208e04896dd21bf
Niladri Saha <sahaniladri27y@gmail.com>
2026-02-25 13:30:03 +0530

    functional component - customer list CRUD operation (project)

---
+++ 05_developing_front-end_apps_with_react/03-functional_component_part-1-customer-list_crud/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import "./css/Header.css";
+
+function Header() {
+  return (
+    <header className="p-3">
+      <h1 className="h1 text-center text-primary fw-semibold">
+        Customer Management System
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
